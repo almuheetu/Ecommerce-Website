6ce185f7763914ff67c98669910bf07d98eee378
Almuheetu Shihab <almuheetu@gmail.com>
2023-09-18 21:56:18 +0600

    .site-off , .off-canvas css fix

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function copyMenu() {
+    // copy inside .dpt-cat to .departments
     var dptCategory = document.querySelector('.dpt-cat');
     var dptPlace = document.querySelector('.departments');
     dptPlace.innerHTML = dptCategory.innerHTML;
@@ -6,7 +7,7 @@ function copyMenu() {
 
 
 
-
+    // copy inside nav to nav
     var mainNav = document.querySelector('.header-nav nav');
     var navPlace = document.querySelector('.off-canvas nav');
     navPlace.innerHTML = mainNav.innerHTML;
@@ -14,10 +15,10 @@ function copyMenu() {
 
 
 
-
+    // copy .header-top .wrapper to .thetop-nav
     var topNav = document.querySelector('.header-top');
     var topPlace = document.querySelector('.off-canvas .thetop-nav');
     topPlace.innerHTML = topNav.innerHTML;
 
 }
-copyMenu()
+copyMenu();
