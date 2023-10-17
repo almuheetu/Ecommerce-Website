37657a638f0e20a65a100a33875181bb59b136c9
Almuheetu Shihab <almuheetu@gmail.com>
2023-10-18 04:36:12 +0600

    js fixed

--- main.js
+++ main.js
@@ -65,8 +65,8 @@ const searchButton = document.querySelector('.t-search'),
     tClose = document.querySelector('.search-close'),
     showClass = document.querySelector('.site');
 searchButton.addEventListener('click', function () {
-    showClass.classList.toggle('.showsearch')
+    showClass.classList.toggle('showsearch')
 });
-tClose.addEventListener('.click', function () {
-    showClass.classList.remove('.showsearch')
+tClose.addEventListener('click', function () {
+    showClass.classList.remove('showsearch')
 });
